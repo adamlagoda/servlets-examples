99b54a3e1a64f9e70968356cc1b06f0e248c2b0b
Adam Łagoda <adam.lagoda@protonmail.com>
2020-09-12 08:22:13 +0200

    Zadanie 4

--- src/main/java/org/sda/web/filters/AuthenticationFilter.java
+++ src/main/java/org/sda/web/filters/AuthenticationFilter.java
@@ -1,18 +1,48 @@
 package org.sda.web.filters;
 
+import org.sda.web.database.dao.SessionsDao;
+
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebFilter(filterName = "AuthenticationFilter", servletNames = {"UsersServlet"})
 public class AuthenticationFilter implements Filter {
+
+    private SessionsDao sessionsDao;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        sessionsDao = new SessionsDao();
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
+        try {
+            long token = 0;
+            long sessionId = 0;
+            Cookie[] cookies = ((HttpServletRequest) request).getCookies();
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if ("SESSION_ID".equals(cookie.getName())) {
+                        sessionId = Long.parseLong(cookie.getValue());
+                    }
+                    if ("TOKEN".equals(cookie.getName())) {
+                        token = Long.parseLong(cookie.getValue());
+                    }
+                }
+            }
+            if (sessionsDao.hasActiveSession(sessionId, token)) {
+                chain.doFilter(request, response);
+            } else {
+                ((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN);
+            }
+        } catch (NumberFormatException e) {
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN);
+        }
     }
 
     @Override
--- src/main/java/org/sda/web/servlets/UserLogout.java
+++ src/main/java/org/sda/web/servlets/UserLogout.java
@@ -1,7 +1,28 @@
 package org.sda.web.servlets;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet(name = "UserLogout", urlPatterns = {"/adminLogout"})
 public class UserLogout extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[] cookies = req.getCookies();
+        for (Cookie cookie : cookies) {
+            if ("SESSION_ID".equals(cookie.getName())) {
+                cookie.setMaxAge(0);
+                resp.addCookie(cookie);
+            }
+            if ("TOKEN".equals(cookie.getName())) {
+                cookie.setMaxAge(0);
+                resp.addCookie(cookie);
+            }
+        }
+    }
 }
